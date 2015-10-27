1e73b5314b67f15f2d686d19c89ac7a3f8bfe576
Michael Hahn <mwhahn@gmail.com>
2015-10-27 13:00:11 -0700

    Update dispatcher code for compatability with Django 1.9
    https://github.com/django/django/blob/1.9b1/django/dispatch/dispatcher.py

--- src/async_signals/dispatcher.py
+++ src/async_signals/dispatcher.py
@@ -1,6 +1,8 @@
+import sys
+
 from celery import task
 from django.dispatch.dispatcher import (
-    _make_id,
+    NO_RECEIVERS,
     Signal,
 )
 
@@ -14,40 +16,75 @@ class AsyncSignal(Signal):
 
     def send(self, sender, **named):
         """Send the signal via Celery."""
+        return self._send.apply_async(
+            args=(sender,),
+            kwargs=named,
+            queue=self.queue,
+        )
 
-        self.propagate_signal.apply_async(
-            args=(sender),
+    def send_robust(self, sender, **named):
+        return self._send_robust.apply_async(
+            args=(sender,),
             kwargs=named,
             queue=self.queue,
         )
 
     @task
-    def propagate_signal(self, sender, **named):
+    def _send(self, sender, **named):
+        """
+        Send signal from sender to all connected receivers.
+        If any receiver raises an error, the error propagates back through send,
+        terminating the dispatch loop, so it is quite possible to not have all
+        receivers called if a raises an error.
+        Arguments:
+            sender
+                The sender of the signal Either a specific object or None.
+            named
+                Named arguments which will be passed to receivers.
+        Returns a list of tuple pairs [(receiver, response), ... ].
+        """
+        responses = []
+        if not self.receivers or self.sender_receivers_cache.get(sender) is NO_RECEIVERS:
+            return responses
+
+        for receiver in self._live_receivers(sender):
+            response = receiver(signal=self, sender=sender, **named)
+            responses.append((receiver, response))
+        return responses
+
+    @task
+    def _send_robust(self, sender, **named):
         """
         Send signal from sender to all connected receivers catching errors.
-
         Arguments:
-
-            sender The sender of the signal. Can be any python object
-                (normally one registered with a connect if you
-                actually want something to occur).
-
+            sender
+                The sender of the signal. Can be any python object (normally one
+                registered with a connect if you actually want something to
+                occur).
             named
                 Named arguments which will be passed to receivers. These
                 arguments must be a subset of the argument names defined in
                 providing_args.
-
         Return a list of tuple pairs [(receiver, response), ... ]. May raise
         DispatcherKeyError.
-
         If any receiver raises an error (specifically any subclass of
         Exception), the error instance is returned as the result for that
-        receiver.
+        receiver. The traceback is always attached to the error at
+        ``__traceback__``.
         """
+        responses = []
+        if not self.receivers or self.sender_receivers_cache.get(sender) is NO_RECEIVERS:
+            return responses
 
         # Call each receiver with whatever arguments it can accept.
-        for receiver in self._live_receivers(_make_id(sender)):
+        # Return a list of tuple pairs [(receiver, response), ... ].
+        for receiver in self._live_receivers(sender):
             try:
-                receiver(signal=self, sender=sender, **named)
-            except Exception:
-                pass
+                response = receiver(signal=self, sender=sender, **named)
+            except Exception as err:
+                if not hasattr(err, '__traceback__'):
+                    err.__traceback__ = sys.exc_info()[2]
+                responses.append((receiver, err))
+            else:
+                responses.append((receiver, response))
+        return responses
